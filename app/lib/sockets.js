9879f4d8fcd79cc02bed2ffe90f6cf40d5811955
Bradley Wogsland <bradley@wogsland.org>
2014-04-08 20:15:24 -0500

    users

--- app/lib/sockets.js
+++ app/lib/sockets.js
@@ -2,4 +2,13 @@
 
 exports.connection = function(socket){
   socket.emit('online', {date: new Date()});
+  socket.on('join', join);
 };
+
+function join(data){
+  console.log('Jeebus! Messages came!');
+  console.log(data);
+  
+  var socket = this;
+  socket.broadcast.emit('joined',data);
+}
--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -5,14 +5,39 @@
   $(document).ready(initialize);
 
   var socket;
-
+  var game = {};
+  
   function initialize(){
     initializeSocketIO();
+	$('#join').click(join);
   }
 
- function initializeSocketIO(){
+  function join(){
+	var user = $('#user').val();
+	game.user = user;
+	game.letters = [];
+	game.nickname = user.concat(" the loser");
+	var tr = '<tr class="primary" data-user="'+user+'"><td class="user">'+user+'</td><td class="letters"></td><td class="nickname">'+game.nickname+'</td></tr>';
+	$('#users > tbody').append(tr);
+	socket.emit('join',{user:user});
+  }
+  
+  function joined(data){
+    console.log('Jeebus! Other PeOpLeS plaaaying!');
+	console.log(data);
+	
+	var user = data.user;
+	game.user = user;
+	game.letters = [];
+	game.nickname = user.concat(" the loser");
+	var tr = '<tr class="secondary" data-user="'+user+'"><td class="user">'+user+'</td><td class="letters"></td><td class="nickname">'+game.nickname+'</td></tr>';
+	$('#users > tbody').append(tr);
+  }
+  
+  function initializeSocketIO(){
     socket = io.connect('/game');
     socket.on('online', function(data){console.log(data);});
+	socket.on('joined', joined);
   }
 
 })();
